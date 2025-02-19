0e6a4ebebc0945d8e8b4be5866d245d3c4c52a56
B0K1NG <dominykaspavlijus@gmail.com>
2025-02-19 15:43:27 +0200

    add NavBar and ShowList components to App for improved layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import ShowList from './components/Showlist'
+import NavBar from './components/Navbar'
+
+
 function App() {
   
 
   return (
     <>
-      Hello
+      <div>
+        <NavBar />
+        <ShowList />
+      </div>
     </>
   )
 }
