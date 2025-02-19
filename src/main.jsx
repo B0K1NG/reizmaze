68519d4ed9025dade14279c3fff42ef19881157a
B0K1NG <dominykaspavlijus@gmail.com>
2025-02-19 12:27:20 +0200

    refactor App component and integrate routing with NotFoundPage

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { BrowserRouter, createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import './styles/style.css'
+
 import App from './App.jsx'
+import NotFoundPage from './components/NotFoundPage.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFoundPage />,
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
