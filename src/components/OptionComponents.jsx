73ce1a876d0958ec55e4fb372124fc02cd45446c
B0K1NG <dominykaspavlijus@gmail.com>
2025-02-20 13:44:04 +0200

    feat: add CheckboxOption and RadioOption components for enhanced select functionality

---
+++ src/components/OptionComponents.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { components } from 'react-select';
+import '../styles/components/_checkbox.scss';
+
+const CheckboxOption = (props) => {
+    return (
+        <components.Option {...props} className="checkbox-option">
+            <input
+                type="checkbox"
+                checked={props.isSelected}
+                onChange={() => null}
+            />{' '}
+            <label>{props.label}</label>
+        </components.Option>
+    );
+};
+
+const RadioOption = (props) => {
+    return (
+        <components.Option {...props} className="radio-option">
+            <input
+                type="radio"
+                checked={props.isSelected}
+                onChange={() => null}
+            />{' '}
+            <label>{props.label}</label>
+        </components.Option>
+    );
+};
+
+export { CheckboxOption, RadioOption };
