9e7bc2624d420304f00b0ae329e8fbe1075074e2
B0K1NG <dominykaspavlijus@gmail.com>
2025-02-20 20:47:53 +0200

    feat: update Navbar component with new logo and theme toggle functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,8 @@
-import reactLogo from '../assets/react.svg';
-import { Link } from 'react-router-dom';
+import reizLogo from '../assets/logo.png';
+import sunIcon from '../assets/sun.png';
+import moonIcon from '../assets/moon.png';
+
+import { Link, useLocation } from 'react-router-dom';
 import { ThemeContext } from '../context/ThemeContext';
 import { useContext } from 'react';
 
@@ -7,19 +10,39 @@ import { useContext } from 'react';
 const NavBar = () => {
 
     const { theme, toggleTheme } = useContext(ThemeContext);
+    const location = useLocation();
 
     return (
         <>
             <nav className={`navbar ${theme}`}>
-                <div className="navbar-left">
-                    <img src={reactLogo} alt="React Logo" />
-                    <button onClick={toggleTheme}>
-                        {theme === 'dark-theme' ? '🌞' : '🌙'}
-                    </button>
+                <div className="navbar__left">
+                    <Link to={'/'}>
+                        <img src={reizLogo} alt="React Logo" className='navbar__logo'/>
+                    </Link>
+                    <div 
+                        className={`theme-slider ${theme === 'light-theme' ? 'light' : ''}`}
+                        onClick={toggleTheme}
+                    >   
+                        <span className="theme-slider__icon theme-slider__icon--moon"><img src={moonIcon} alt="Dark" /></span>
+                        <div className="theme-slider__button"></div>
+                        <span className="theme-slider__icon theme-slider__icon--sun"><img src={sunIcon} alt="Light" /></span>
+                        
+                        
+                    </div>
                 </div>
-                <div className='navbar-right'>
-                    <Link to={'/'}>HOME</Link>
-                    <Link to={'/favorites'}>FAVORITES</Link>
+                <div className='navbar__right'>
+                    <Link 
+                        to={'/'}
+                        className={location.pathname === '/' ? 'active' : ''}
+                    >
+                        HOME
+                    </Link>
+                    <Link 
+                        to={'/favorites'}
+                        className={location.pathname === '/favorites' ? 'active' : ''}
+                    >
+                        FAVORITES
+                    </Link>
                 </div>
             </nav>
         </>
