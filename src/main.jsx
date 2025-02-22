2233244c331d41959cacceaf9a21d02e84039817
B0K1NG <dominykaspavlijus@gmail.com>
2025-02-22 18:12:04 +0200

    feat: delete unused browserrouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 import './styles/style.css';
 
