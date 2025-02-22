cd4edcaade2d0c1c2431d1808b2b92ec0dc95f24
B0K1NG <dominykaspavlijus@gmail.com>
2025-02-22 16:33:53 +0200

    feat: update to NotFoundPage

--- src/components/NotFoundPage.jsx
+++ src/components/NotFoundPage.jsx
@@ -1,20 +1,26 @@
+import notFoundImg from '../assets/not-found.png';
 import { useNavigate } from "react-router-dom";
 
 const NotFoundPage = () => {
 
-        const navigate = useNavigate();
+    const navigate = useNavigate();
 
-        const handleHome = () => {
-            navigate('/');
-        }
+    const handleHome = () => {
+        navigate('/');
+    }
 
     return (
-    <>
-    <h1>404 Page</h1>
-    <button onClick={handleHome}>Go Back Home</button>
-    </>
-    )
-
+        <div className="not-found-page">
+        <div className="not-found-page__content">
+            <h1>Channel Not Found!</h1>
+            <p>Looks like we're experiencing technical difficulties. 
+            This page is currently unavailable on all frequencies.
+            </p>
+            <button onClick={handleHome}>Return to Regular Browsing→</button>
+        </div>
+        <img src={notFoundImg} alt="Not Found" className="not-found-page__image" />
+        </div>
+    );
 };
 
 export default NotFoundPage;
